e025db80b3404adb98bc9e8bf1ad5cc1540b83e6
pritidemo <103582175+pritidemo@users.noreply.github.com>
2022-05-12 18:45:07 +0530

    Create Practice2.java

---
+++ Practice2.java
@@ -0,0 +1,29 @@
+package Loops_Pack;
+
+public class Practice2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int r,i,s=0,num1;
+		
+		for(i=1;i<=1000;i++)
+		{
+			num1=i;
+			s=0;
+			while(num1>0)
+			{
+				r=num1%10;
+				s=s+r*r*r;
+				num1=num1/10;
+			}
+			if(i==s)
+			{
+				System.out.println(s);
+			}
+			
+		}
+		
+	}
+
+}
